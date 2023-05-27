e61170494a91bcab4936a2ecb856b9b12fc2479d
vflores <vflores2002@estudiantec.cr>
2023-05-27 12:15:22 -0600

    primer commit de Vidal

---
+++ AirWar_/src/Clases/Portaaviones.java
@@ -0,0 +1,17 @@
+/**
+ * Clase encargada de crear los aeropuertos
+ */
+class Portaaviones {
+    private String nombre;
+    private int capacidadCombustible;
+
+    public Portaaviones(String nombre, int capacidadCombustible) {
+        this.nombre = nombre;
+        this.capacidadCombustible = capacidadCombustible;
+    }
+
+
+
+    // Otros métodos y getters/setters
+
+}
---
+++ AirWar_/src/Clases/Ruta.java
@@ -0,0 +1,19 @@
+/**
+ * Clase que crea las rutas posibles
+ */
+class Ruta {
+    private String origen;
+    private String destino;
+    private int distancia;
+    private int peso;
+
+    public Ruta(String origen, String destino, int distancia, int peso) {
+        this.origen = origen;
+        this.destino = destino;
+        this.distancia = distancia;
+        this.peso = peso;
+    }
+
+    // Otros métodos y getters/setters
+
+}
