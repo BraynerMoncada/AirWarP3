6f831471168483baf9c80bc2868334e6f150a5a0
ThDylanJ0S3 <dilanmezamarin@gmail.com>
2023-05-27 19:49:11 -0600

    Se añadio un menu de inicio y clases que se utilizaran en el juego

--- src/controller/main.java
+++ src/controller/main.java
@@ -13,7 +13,9 @@ import javafx.stage.Stage;
 /**
  * Metodo principal que corre el juego
  *
- *@author Vidal Flores
+ * @author Vidal Flores
+ * @author Dylan Meza
+ * @author Brayner Moncada
  */
 public class main extends Application {
 
@@ -38,29 +40,13 @@ public class main extends Application {
      */
     public void start(Stage stage) throws Exception {
         
-        javafx.geometry.Rectangle2D pantalla = Screen.getPrimary().getVisualBounds();
-
-        
-        Parent rootMenuJuego = FXMLLoader.load(getClass().getResource("/vista/menuPrincipalJuego.fxml"));
+        Parent rootMenuJuego = FXMLLoader.load(getClass().getResource("/vista/menuPlay.fxml"));
         Scene sceneMenuJuego = new Scene(rootMenuJuego);
         Stage stageMenuJuego = new Stage();
         stageMenuJuego.setTitle("Menú principal");
         stageMenuJuego.setScene(sceneMenuJuego);
-        stageMenuJuego.setX(pantalla.getMinX() + 100); // Colocar la ventana a 100 píxeles de la izquierda
-        stageMenuJuego.setY(pantalla.getMinY() + 100); // Colocar la ventana a 100 píxeles desde la parte superior
-
-        Parent rootControlador = FXMLLoader.load(getClass().getResource("/vista/menuPrincipalControladorAereo.fxml"));
-        Scene sceneControlador = new Scene(rootControlador);
-        Stage stageControlador = new Stage();
-        stageControlador.setTitle("Menú principal Controlador Aereo");
-        stageControlador.setScene(sceneControlador);
-        stageControlador.setX(stageMenuJuego.getX() + 620); // Colocar la ventana a la derecha del maestro con un espacio de 20 píxeles
-        stageControlador.setY(pantalla.getMinY() + 100); // Colocar la ventana a la misma altura que el maestro
-        
-        
+       
         stageMenuJuego.show();
-        stageControlador.show();
-
 
 /*
         Servidor servidor = new Servidor(8080);
